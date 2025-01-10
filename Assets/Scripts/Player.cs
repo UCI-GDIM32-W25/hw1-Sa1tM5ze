d8832570e6e9c4e8d328510fb5c4f35ffad5ba1f
Sa1tM5ze <158608560+Sa1tM5ze@users.noreply.github.com>
2025-01-09 21:05:11 -0800

    Got all the mechanics to work properly
    no builds have been built yet; need to add devlog also

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,6 @@
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
@@ -13,16 +15,60 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        //refresh the seed counts when the game starts
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+
+        //update the game ui when the game starts
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
+        //player movements up, down, left, right
+        if (Input.GetKey(KeyCode.W))
+        {
+            _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
+        }
         
+        if (Input.GetKey(KeyCode.S))
+        {
+            _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _playerTransform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
+
+
+        //player planting seeds
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            //call the plantseed method if there are remaining seeds
+            if (_numSeeds > 0)
+            {
+                PlantSeed();
+                _numSeeds--;
+                Debug.Log("you have planted one seed! good job solving global warming");
+            }
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        //increase the num of planted seeds, decrease the num of remaining seeds
+        _numSeedsPlanted ++;
+        _numSeedsLeft--;
+        //call and update the UI
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+
+        //spawning the seed
+        GameObject plant = Instantiate(_plantPrefab, transform.position, transform.rotation);
     }
 }
